87796433e1ef5a11298f150491df7e16b6f72c83
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-19 21:43:16 -0300

    Introduction to Computers, Programs, and Java.

---
+++ Chapter01/Exercise1_12.java
@@ -0,0 +1,8 @@
+package Chapter1;
+//1 mile = 1.6 kilometer
+public class Exercise1_12 {
+    public static void main(String[] args) {
+        System.out.print("The average speed in kilometers per hour is = ");
+        System.out.println( (24 * 1.6) / (1 + (((40.0 + (35.0 / 60)) / 60))) );
+    }
+}
