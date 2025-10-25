e3edafd4b60c63e14a6073697b78b884a4928708
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-25 14:21:06 -0300

    Elementary Programming

--- Chapter02/CaseStudy/CurrentTimeInGMT.java
+++ Chapter02/CaseStudy/CurrentTimeInGMT.java
@@ -16,28 +16,8 @@ public class CurrentTimeInGMT {
         long currentMinutes = minutes % 60;
         long currentSeconds = seconds % 60;
 
-        //Printing the current time as 05:09:08 instead of 5:9:8 (5 hours, 9 minutes and 8 seconds).
-        //Hours
-        if (currentHour < 10) {
-            System.out.println("The current time is: 0" + currentHour + ":" + currentMinutes + ":" + currentSeconds);
-        }
-        else {
-            System.out.println("The current time is: " + currentHour + ":" + currentMinutes + ":" + currentSeconds);
-        }
-        //Minutes
-        if (currentMinutes < 10) {
-            System.out.println("The current time is: " + currentHour + ":0" + currentMinutes + ":" + currentSeconds);
-        }
-        else {
-            System.out.println("The current time is: " + currentHour + ":" + currentMinutes + ":" + currentSeconds);
-        }
-        //Seconds
-        if (currentSeconds < 10) {
-            System.out.println("The current time is: " + currentHour + ":" + currentMinutes + ":0" + currentSeconds);
-        }
-        else {
-            System.out.println("The current time is: " + currentHour + ":" + currentMinutes + ":" + currentSeconds);
-        }
+        //Printing the current time
+        System.out.println("The current time is: " + currentHour + ":" + currentMinutes + ":" + currentSeconds);
 
     }
 
