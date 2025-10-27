d76152864c436e03cd542e1c67163ea69a15f361
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-27 15:23:25 -0300

    Elementary Programming

--- Chapter02/CaseStudy/CountingMonetaryUnits.java
+++ Chapter02/CaseStudy/CountingMonetaryUnits.java
@@ -7,40 +7,47 @@ public class CountingMonetaryUnits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //Prompt the inputs to the user
+        //Prompt the user for input
         System.out.println("This converter will ask you to enter two values:\n" +
-                           "An integer value for dollars and another for cents.\n" +
-                           "The converter will then return the equivalent amount in dollars, quarters, dimes, nickels and cents.\n");
-        //Prompt for the input1
+                "An integer value for dollars and another for cents.\n" +
+                "The converter will then return the equivalent amount\n" +
+                "in dollars, quarters, dimes, nickels and cents, in a way that\n" +
+                "minimizes the total number of coins.\n");
+        //Prompt for the first input
         System.out.print("Enter an integer number of dollars to convert: $");
         long input1 = input.nextLong();
-        //Prompt for the input2
+        //Prompt for the second input
         System.out.print("Enter an integer number of cents to convert: ¢");
         long input2 = input.nextLong();
 
-        //Defining the variables to display for the user
-        //dollars
+        //Define the variables to display to the user
+        //Dollars
         long dollars = (input1 * 100 + input2) / 100;
+        long initialRemainingCents = (input1 * 100 + input2) % 100;
         long remainingCents = (input1 * 100 + input2) % 100;
-        //quarters
-        long quarters = (input1 * 100 + input2) / 25;
-        //dimes
-        long dimes = (input1 * 100 + input2) / 10;
-        //nickels
-        long nickels = (input1 * 100 + input2) / 5;
-        //pennies
-        long pennies = (input1 * 100 + input2);
+        //Quarters
+        long quarters = remainingCents / 25;
+        remainingCents = remainingCents % 25;
+        //Dimes
+        long dimes = remainingCents / 10;
+        remainingCents = remainingCents % 10;
+        //Nickels
+        long nickels = remainingCents / 5;
+        remainingCents = remainingCents % 5;
+        //Pennies
+        long pennies = remainingCents;
 
 
-        //Display to the user the report
+        //Display the report to the user
         System.out.println("\nYou entered: $" + (input1 * 100 + input2) / 100.0 + " (" + dollars +
-                           " dollars and " + remainingCents + " cemts)\n");
+                " dollars and " + initialRemainingCents + " cents)\n");
         System.out.println("Your value converted is: ");
-        System.out.println("In dollars: " + dollars + " Dollars");
-        System.out.println("In quarters: " + quarters + " Quarters");
-        System.out.println("In dimes: " + dimes + " Dimes");
-        System.out.println("In nickels: " + nickels + " Nickels");
-        System.out.println("In cents: " + pennies + " Cents");
+        System.out.println("Dollars: " + dollars + " Dollars");
+        System.out.println("Quarters: " + quarters + " Quarters");
+        System.out.println("Dimes: " + dimes + " Dimes");
+        System.out.println("Nickels: " + nickels + " Nickels");
+        System.out.println("Cents: " + pennies + " Cents");
 
     }
+
 }
