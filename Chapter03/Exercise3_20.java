201cd35206991e9d5d1b2683288c669f4e38b7eb
RenatoMcln <renatodeveloper17@gmail.com>
2025-11-12 10:42:02 -0300

    Selections

---
+++ Chapter03/Exercise3_20.java
@@ -0,0 +1,39 @@
+package Chapter03;
+
+import java.util.Scanner;
+
+public class Exercise3_20 {
+
+    //Science: wind-chill temperature
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //prompts the user to enter a temperature and a wind speed
+        System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F: ");
+        double fahrenheit = input.nextDouble();
+
+        if ((fahrenheit >= 41 || fahrenheit <= -58)) {
+            System.out.println("Invalid fahrenheit value!");
+            System.exit(1);
+        }
+
+        System.out.print("Enter the wind speed (>= 2) in miles per hour: ");
+        double windSpeed = input.nextDouble();
+
+        if (windSpeed < 2) {
+            System.out.println("Invalid windSpeed value!");
+            System.exit(1);
+        }
+
+        //Calculating wind-chill temperature
+        double windChillTemp = (35.74 + 0.6215 * fahrenheit - 35.75 * Math.pow(windSpeed, 0.16) +
+                0.4275 * fahrenheit * Math.pow(windSpeed, 0.16));
+
+        double windChillTempRounded = (int) (windChillTemp * 10000) / 10000.0;
+
+        //Display the result to the user
+        System.out.println("The wind chill index is " + windChillTempRounded);
+
+
+    }
+}
